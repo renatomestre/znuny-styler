98812eaf1e758d45e951386ebea5e2ec2d8ce07f
RENATO DE MOURA MESTRE <RMESTRE@trf3.jus.br>
2026-09-18 16:44:11 -0300

    Turn toolbar always visible on top

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
     > git clone \<url>
 
-2. Run multi-browser package:
+2. For each add, modify or delete in files, run multi-browser package again:
 
     > python .\package.py
 
-3. Import from specific browser in 'dist' folder
+3. Import from specific browser in 'dist' folder or reload the extension/addon on browser
 
 Made with 💚 and 🤖
--- background.js
+++ background.js
@@ -3,23 +3,27 @@
 
   console.log("znuny-styler: background script loaded");
 
-  // let customStyle = `
-  //   :root {
-  //     --font-size-sm: 10px !important;
-  //     --main-font-size: 11px !important;
-  //     --padding-xs: 2px !important;
-  //     --padding-sm: 2px !important;
-  //     --padding-md: 2px !important;
-  //   }
-
-  //   .Handle.ui-resizable-handle {
-  //     background: white !important;
-  //   }
-  // `;
-
-  function setVariables() {
+  function run() {
     if (!document.documentElement) {
       return;
+    }
+
+    const navigation = document.getElementById("Navigation");
+    const toolbar = document.getElementById("ToolBar");
+    const toolbarRow = document.querySelector(".toolbar-row");
+    const toolbarToggle = document.getElementById("ToolBar-toggle");
+
+    toolbarRow?.style.setProperty("border", "none", "important");
+    toolbarToggle?.style.setProperty("display", "none", "important");
+
+    if (navigation && toolbar) {
+      toolbar.style.setProperty("float", "left", "important");
+      toolbar.style.setProperty("margin-top", "0px", "important");
+      toolbar.querySelectorAll(":scope li").forEach((element) => {
+        element.style.setProperty("float", "inline-end", "important");
+      });
+
+      navigation.appendChild(toolbar);
     }
 
     document.documentElement.style.setProperty("--font-size-sm", "10px", "important");
@@ -60,8 +64,7 @@
 
     extensionApi.scripting.executeScript({
       target: { tabId },
-      // css: customStyle
-      func: setVariables
+      func: run
     }).catch((error) => {
       console.error("znuny-styler: could not inject script", error);
     });
